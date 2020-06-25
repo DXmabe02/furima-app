744389dd2d89ea7a0fffceccbd1c70937d8eb41e
honshin4n1 <million_films.sh@icloud.com>
2020-06-25 09:53:34 +0900

    add action purchase pay routes get purchase

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,9 @@ class ItemsController < ApplicationController
     
   end
   
+  def purchase
+  end
+  
+  def pay
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
+  
+  resources :items do
+    collection do
+      get 'purchase'
+    end
+  end
+
 end
