74dc8f77254c6b947a2f10d1fff4c23a779501d4
mnboogaloo <boomana1957@gmail.com>
2020-06-29 10:20:21 +0900

    commi/css

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,14 @@
 class ItemsController < ApplicationController
 
   def index
-    @new = Item.all
+    @new = Item.includes(:item_images).order('created_at DESC')
   end
 
   def new
     @item = Item.new
     # レイヤーを変更
     # render layout: "nothing"
-  end
-
-  def create
+    @item.item_images.new
   end
 
   def show
@@ -26,12 +24,17 @@ class ItemsController < ApplicationController
 
   def create
     Item.create(item_params)
+    # if @item.save
+    #   redirect_to root_path
+    # else
+    #   render :new
+    # end
     redirect_to root_path
   end
 
   private
   def item_params
-    params.require(:item).permit(:name, :introduction, :condition, :price, :size, :prefecture_id, :preparation_day_id, :postage_payer_id)
+    params.require(:item).permit(:name, :introduction, :condition, :price, :size, :prefecture_id, :preparation_day_id, :postage_payer_id, item_images_attributes: [:image])
     # params.require(:item).permit(:prefecture_id, :preparation_day_id, :postage_payer_id)
 
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,8 @@ class Item < ApplicationRecord
   # belongs_to :user
   # has_one :postage_payer
   # has_one :preparation_day
+  has_many :item_images
+  accepts_nested_attributes_for :item_images, allow_destroy: true
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :preparation_day
   belongs_to_active_hash :postage_payer
